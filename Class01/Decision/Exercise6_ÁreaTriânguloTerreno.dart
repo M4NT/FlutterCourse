e09e1c176b4771dcb64af05e810208bc254fbd9e
Yan Mantovani <101876537+M4NT@users.noreply.github.com>
2022-07-31 21:09:49 -0300

    Create Exercise6_ÁreaTriânguloTerreno.dart

---
+++ Class01/Decision/Exercise6_ÁreaTriânguloTerreno.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Home());
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController n1Controller = TextEditingController();
+  TextEditingController n2Controller = TextEditingController();
+
+  String infoResultado = "Informe os números do retângulo!";
+
+  void _calcularBase() {
+    setState(() {
+      double n1 = double.parse(n1Controller.text);
+      double n2 = double.parse(n2Controller.text);
+
+      double resultado = n1 * n2;
+
+      if (resultado > 100) {
+        infoResultado = 'Resultado: $resultado \n Terreno grande.';
+      } else {
+        infoResultado = 'Resultado: $resultado \n Terreno pequeno';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: _titulo(),
+        backgroundColor: Colors.white,
+        body: _corpo(),
+      ),
+    );
+  }
+
+  _titulo() {
+    return AppBar(
+      title: Text("Área do retângulo"),
+      centerTitle: true,
+      backgroundColor: Colors.green,
+    );
+  }
+
+  _corpo() {
+    return SingleChildScrollView(
+      padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          _campo("Digite a base do retângulo", n1Controller),
+          _campo("Digite a altura do retângulo", n2Controller),
+          _botao(),
+          _texto(infoResultado),
+        ],
+      ),
+    );
+  }
+
+  _campo(labelTitulo, objController) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+          labelText: labelTitulo, labelStyle: TextStyle(color: Colors.green)),
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.green, fontSize: 25.0),
+      controller: objController,
+    );
+  }
+
+  _botao() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+      child: Container(
+        height: 50.0,
+        child: RaisedButton(
+          onPressed: _calcularBase,
+          child: Text("Calcular",
+              style: TextStyle(color: Colors.white, fontSize: 20.0)),
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+
+  _texto(textoParaExibir) {
+    return Text(
+      textoParaExibir,
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.red, fontSize: 25.0),
+    );
+  }
+}
